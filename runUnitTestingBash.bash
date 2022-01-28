0e8e0ed41704185a8545839a068bdb7f2949685d
Johann Joubert <Johann.joubert@pal-v.com>
2022-01-28 16:54:45 +0100

    Added more exclusion to project and updated comments in BuildPorjectOutputs

--- runUnitTestingBash.bash
+++ runUnitTestingBash.bash
@@ -48,7 +48,9 @@ $BULLSEYE_LOC/cov01 --on
 make -e CC="$BULLSEYE_LOC/covc -i $BULLSEYE_LOC/gcc" -B $1
 ./$1 xml out
 $BULLSEYE_LOC/cov01 --off
-$BULLSEYE_LOC/covselect --file "%COVFILE%" --add '!/CUnit/src/'
+$BULLSEYE_LOC/covselect --file "%COVFILE%" --add '!../CUnit/'
+$BULLSEYE_LOC/covselect --file "%COVFILE%" --add '!../Test/'
+$BULLSEYE_LOC/covselect --file "%COVFILE%" --add '!../Drivers'
 $BULLSEYE_LOC/covselect --file "%COVFILE%" --list
 echo START BULLSEYE HTML REPORT
 /opt/bullshtml/bullshtml -f $COVFILE cov_html_output
