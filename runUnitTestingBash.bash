005dd6935284c67433806453dcb6f2a1d787f154
Johann Joubert <Johann.joubert@pal-v.com>
2022-01-28 14:32:20 +0100

    created runUnitTestingBash.bash script to switch to correct project and configuration and run UnitTesting outputs.

---
+++ runUnitTestingBash.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "set PC-LINT configuration"
+
+#PC LINT CONFIGURATION
+export PC_LINT_LOC=/opt/PC-LINT/config
+export PC_LINT_LNT_LOC=/opt/PC-LINT/lnt
+export PC_LINT_PROJECT_CONFIG=./project_config.lnt
+
+#Temporary filename for imposter log
+export IMPOSTER_LOG=./imposter_log.txt
+export PC_LINT_ANALYSIS_FILE=./analysis.log
+
+#Bullseye Configuration
+export BULLSEYE_LOC=/opt/BullseyeCoverage/bin
+export COVFILE=Test.cov
+export COV_HTML_OUTPUT=./cov_html_output
+export PATH=$PATH:$BULLSEYE_LOC
+
+echo $PATH
+
+
+echo $PC_LINT_LOC
+echo $PC_LINT_PROJECT_CONFIG
+echo $IMPOSTER_LOG
+echo $PC_LINT_ANALYSIS_FILE
+
+echo "commandline paramters"
+echo "Project name : "$1
+echo "Configuration name : "$2
+
+
+#switch to UnitTesting output folder of supplied project and configuration -> could hardcode UnitTesting in path.
+cd $1/$2/
+
+
+#cleanup files
+rm $PC_LINT_PROJECT_CONFIG
+rm $PC_LINT_ANALYSIS_FILE
+rm $IMPOSTER_LOG
+rm $COVFILE
+rm $IMPOSTER_LOG
+#del %PROJECT_CONFIG%
+
+rm -rf /s /q %COV_HTML_OUTPUT%
+
+$BULLSEYE_LOC/cov01 --on
+make -e CC="$BULLSEYE_LOC/covc -i $BULLSEYE_LOC/gcc" -B $1
+./$1 xml out
+$BULLSEYE_LOC/cov01 --off
+#TEMP this does not work yet $BULLSEYE_LOC/covselect --file "%COVFILE%" --add "!..\CUnit\"
+echo START BULLSEYE HTML REPORT
+/opt/bullshtml/bullshtml -f $COVFILE cov_html_output
+echo STOP BULLSEYE HTML REPORT
